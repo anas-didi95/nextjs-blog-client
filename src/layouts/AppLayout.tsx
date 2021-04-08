5c4a5b954887ebd557226fcb44012cc25485a1f9
Anas Juwaidi <anas.didi95@gmail.com>
2021-04-08 20:30:40 +0800

    Add metadata constants

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,10 +1,11 @@
 import Head from "next/head"
 import React, { ReactNode } from "react"
+import metadata from "../utils/constants/metadata.json"
 
 const AppLayout: React.FC<{ children: ReactNode }> = ({ children }) => (
   <>
     <Head>
-      <title>Anas Juwaidi's Blog</title>
+      <title>{metadata.title}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <main className="section">{children}</main>
