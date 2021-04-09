7a95abc5fe924bce3ce712e5d928cc58496e4257
Anas Juwaidi <anas.didi95@gmail.com>
2021-04-09 20:57:01 +0800

    Run formatter

--- src/components/ProfileInfo.tsx
+++ src/components/ProfileInfo.tsx
@@ -3,8 +3,7 @@ import Image from "next/image"
 import metadataJson from "../utils/constants/metadata.json"
 import styles from "../../styles/ProfileInfo.module.css"
 
-const ProfileInfo: React.FC<{}> = () =>
-(
+const ProfileInfo: React.FC<{}> = () => (
   <>
     <figure className={`image mb-4 ${styles.image}`}>
       <Image
@@ -15,7 +14,7 @@ const ProfileInfo: React.FC<{}> = () =>
         className="is-rounded"
       />
     </figure>
-    <div className={`mb-5 ${styles.label}`} >
+    <div className={`mb-5 ${styles.label}`}>
       <p className="title is-4">{metadataJson.fullname}</p>
       <p className="subtitle is-5">{metadataJson.position}</p>
     </div>
