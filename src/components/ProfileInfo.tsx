60c94266a8e5be233deda8f99ce4f2a84de2a449
Anas Juwaidi <anas.didi95@gmail.com>
2021-04-09 20:27:17 +0800

    Refactor constants

--- src/components/ProfileInfo.tsx
+++ src/components/ProfileInfo.tsx
@@ -1,26 +1,24 @@
 import React from "react"
 import Image from "next/image"
+import metadataJson from "../utils/constants/metadata.json"
 
-const ProfileInfo: React.FC<{}> = () => {
-
-  return (
-    <>
-      <figure className="image is-128x128 mb-5">
-        <Image
-          src="/images/profile-pic.jpg"
-          alt="Profile picture"
-          width={128}
-          height={128}
-          className="is-rounded"
-        />
-      </figure>
-      <div className="mb-5">
-        <p className="title is-4">Anas Juwaidi Bin Mohd Jeffry</p>
-        <p className="subtitle is-5">Software Engineer</p>
-      </div>
-
-    </>
-  )
-}
+const ProfileInfo: React.FC<{}> = () =>
+(
+  <>
+    <figure className="image is-128x128 mb-5">
+      <Image
+        src="/images/profile-pic.jpg"
+        alt="Profile picture"
+        width={128}
+        height={128}
+        className="is-rounded"
+      />
+    </figure>
+    <div className="mb-5">
+      <p className="title is-4">{metadataJson.fullname}</p>
+      <p className="subtitle is-5">{metadataJson.position}</p>
+    </div>
+  </>
+)
 
 export default ProfileInfo
