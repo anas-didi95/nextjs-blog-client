c6d23679b4abcc9b5c52557249504470c1ab369d
Anas Juwaidi <anas.didi95@gmail.com>
2021-04-15 17:10:39 +0800

    Add property href to pass link href

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -1,16 +1,17 @@
 import React, { ReactNode } from "react"
 import Icon from "./Icon"
+import metadataJson from "../utils/constants/metadata.json"
 
 const SocialLink: React.FC<{}> = () => (
   <div className="buttons">
-    <_Link><Icon type="email" /></_Link>
-    <_Link><Icon type="linkedin" /></_Link>
-    <_Link><Icon type="github" /></_Link>
+    <_Link href={`mailto:${metadataJson.social.email}`}><Icon type="email" /></_Link>
+    <_Link href={metadataJson.social.linkedin}><Icon type="linkedin" /></_Link>
+    <_Link href={metadataJson.social.github}><Icon type="github" /></_Link>
   </div>
 )
 
-const _Link: React.FC<{ children: ReactNode }> = ({ children }) => (
-  <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href="https://github.com">
+const _Link: React.FC<{ children: ReactNode, href: string }> = ({ children, href }) => (
+  <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href={href}>
     {children}
   </a>
 )
