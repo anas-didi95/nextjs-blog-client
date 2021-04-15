16df6ca903a679ac5b83b6e53b52c247d9cbc32f
Anas Juwaidi <anas.didi95@gmail.com>
2021-04-15 17:07:28 +0800

    Refactor link

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -1,12 +1,18 @@
-import React from "react"
+import React, { ReactNode } from "react"
 import Icon from "./Icon"
 
 const SocialLink: React.FC<{}> = () => (
   <div className="buttons">
-    <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href="https://github.com"><Icon type="email" /></a>
-    <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href="https://github.com"><Icon type="linkedin" /></a>
-    <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href="https://github.com"><Icon type="github" /></a>
+    <_Link><Icon type="email" /></_Link>
+    <_Link><Icon type="linkedin" /></_Link>
+    <_Link><Icon type="github" /></_Link>
   </div>
+)
+
+const _Link: React.FC<{ children: ReactNode }> = ({ children }) => (
+  <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href="https://github.com">
+    {children}
+  </a>
 )
 
 export default SocialLink
