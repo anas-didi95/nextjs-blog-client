9e223d0e761be889717415519afc735b774e75e3
Anas Juwaidi <anas.didi95@gmail.com>
2021-04-15 17:04:47 +0800

    Add component Icon

---
+++ src/components/Icon.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { IoMail, IoLogoLinkedin, IoLogoGithub } from "react-icons/io5"
+
+interface IIcon {
+  type: "email" | "linkedin" | "github"
+}
+const Icon: React.FC<IIcon> = ({ type }) => (
+  <>
+    {{
+      "email": <IoMail />,
+      "linkedin": <IoLogoLinkedin />,
+      "github": <IoLogoGithub />
+    }[type]}
+  </>
+)
+
+export default Icon
--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -1,11 +1,11 @@
 import React from "react"
-import { IoMail, IoLogoLinkedin, IoLogoGithub } from "react-icons/io5"
+import Icon from "./Icon"
 
 const SocialLink: React.FC<{}> = () => (
   <div className="buttons">
-    <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href="https://github.com"><IoLogoGithub /></a>
-    <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href="https://github.com"><IoLogoLinkedin /></a>
-    <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href="https://github.com"><IoMail /></a>
+    <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href="https://github.com"><Icon type="email" /></a>
+    <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href="https://github.com"><Icon type="linkedin" /></a>
+    <a className="button is-light is-outlined is-inverted is-rounded is-size-6 p-3" href="https://github.com"><Icon type="github" /></a>
   </div>
 )
 
