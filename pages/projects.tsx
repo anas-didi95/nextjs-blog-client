fc23e5280d8bf87494ba6c71dbd0f9c1afe87cb4
Anas Juwaidi <anas.didi95@gmail.com>
2021-04-17 17:14:59 +0800

    Setup layout

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,13 @@
 import React from "react"
+import AppLayout from "../src/layouts/AppLayout"
+import CommonLayout from "../src/layouts/CommonLayout"
 
 const ProjectsPage: React.FC<{}> = () => (
-  <div>hello</div>
+  <AppLayout>
+    <CommonLayout active="projects">
+      <div>Project</div>
+    </CommonLayout>
+  </AppLayout>
 )
 
 export default ProjectsPage
